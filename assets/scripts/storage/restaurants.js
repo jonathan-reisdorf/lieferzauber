f18fa33011fb40c1228ed4cdffae0dccc1e4751b
Jonathan Reisdorf <jonathan.reisdorf@project-a.com>
2015-08-13 16:14:14 +0200

    generating (hopefully) valid meals

--- assets/scripts/storage/restaurants.js
+++ assets/scripts/storage/restaurants.js
@@ -42,5 +42,70 @@ module.exports = ['CommonRequest', 'StorageService',  function(CommonRequest, St
     }, {});
   };
 
+  self.mealItems = {
+    generate : function(menu, isMain, config, tryCount) {
+      if (!menu || !menu.sections || !config) {
+        return null;
+      }
+
+      var selectedTier = (menu.popular_items && Math.random() < config.popularityChance) ?
+        menu.popular_items :
+        menu.sections[Math.round(Math.random() * (menu.sections.length - 1))].items;
+
+      var selectedItems = selectedTier.filter(function(menuItem) {
+        return (isMain ? menuItem.main_item : true);
+      });
+
+      if (!selectedItems.length) {
+        return tryCount > 10 ? null : this.generate(menu, isMain, config, tryCount + 1);
+      }
+
+      var selectedItem = selectedItems[Math.round(Math.random() * (selectedItems.length - 1))];
+      return this.specify(config, angular.copy(selectedItem));
+    },
+    specify : function(config, mealItem, branch) {
+      if (!branch && !mealItem.priceToBuy) {
+        mealItem.priceToBuy = 0;
+        mealItem.quantity = 1;
+      }
+
+      if (!branch) {
+        branch = mealItem;
+      }
+
+      if (branch.sizes && branch.sizes.length) {
+        branch.size = branch.sizes[Math.ceil((branch.sizes.length - 1) * config.preferedMealSizeFactor)];
+        mealItem.priceToBuy += branch.size.price;
+      }
+
+      if (branch.sizes) {
+        delete branch.sizes;
+      }
+
+      if (branch.flavors) {
+        var pickedItem;
+        switch (branch.flavors.structure) {
+          case '-1':
+            branch.flavors.items.map(function(flavorItem) {
+              return self.mealItems.specify(config, mealItem, flavorItem);
+            });
+            break;
+          case '0':
+            pickedItem = branch.flavors.items[Math.round(Math.random() * (branch.flavors.items.length - 1))];
+            branch.flavors.items = Math.random() < config.fancyFlavorChance ? [self.mealItems.specify(config, mealItem, pickedItem)] : [];
+            break;
+          case '1':
+            pickedItem = branch.flavors.items[Math.round(Math.random() * (branch.flavors.items.length - 1))];
+            branch.flavors.items = [self.mealItems.specify(config, mealItem, pickedItem)];
+            break;
+          default:
+            delete branch.flavors;
+        }
+      }
+
+      return branch;
+    }
+  };
+
   return self;
 }];
